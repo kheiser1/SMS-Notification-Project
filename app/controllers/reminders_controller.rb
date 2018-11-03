ffc6450d7df5195ca5d9a7c0f70a15dcd3b94144
Kristopher Heiser <kheiser1@uncc.edu>
2018-11-03 20:26:54 +0000

    Index Page now lists all reminders

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,15 +1,22 @@
 class RemindersController < ApplicationController
     def new
     end
+    
     def create
         @reminder = Reminder.new(reminder_params)
         @reminder.save
         redirect_to @reminder
     end
+    
     def show
         @reminder = Reminder.find(params[:id])
     end
+    
+    def index
+        @reminders = Reminder.all   
+    end
 end
+
 private
 def reminder_params
     params.require(:reminder).permit(:title, :date, :time, :notes)
