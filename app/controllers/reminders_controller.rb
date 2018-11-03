c846abd324972a969731a1db446db6fcbf761bb6
Ryan <rmatth16@uncc.edu>
2018-11-03 00:13:06 +0000

    Adding show/create method

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,4 +1,16 @@
 class RemindersController < ApplicationController
     def new
     end
+    def create
+        @reminder = Reminder.new(reminder_params)
+        @reminder.save
+        redirect_to @reminder
+    end
+    def show
+        @reminder = Reminder.find(params[:id])
+    end
+end
+private
+def reminder_params
+    params.require(:reminder).permit(:title, :date, :time, :notes)
 end
