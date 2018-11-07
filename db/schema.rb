466417836af7e6436057ecddd3a8fee8707bd82b
Ryan <rmatth16@uncc.edu>
2018-11-07 21:10:26 +0000

    Authentication System

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_231959) do
+ActiveRecord::Schema.define(version: 2018_11_06_220249) do
 
   create_table "reminders", force: :cascade do |t|
     t.string "title"
     t.date "date"
     t.time "time"
     t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
