f9578d56ba6c51bb95c8e216f953f1d69fc7dc05
Kristopher Heiser <kheiser1@uncc.edu>
2018-11-12 19:25:56 +0000

    Password Requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
     has_secure_password
+    validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..40},
+                       :on => :create
+    validates :password, :confirmation => true,
+                       :length => {:within => 6..40},
+                       :allow_blank => true,
+                       :on => :update    
     has_many :reminders, :class_name => "Reminder"
 end
