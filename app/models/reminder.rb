020f230642b50771f4216577a7dd2153ff439228
Kristopher Heiser <kheiser1@uncc.edu>
2018-11-12 19:14:10 +0000

    Re-added reminder validation.

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,4 +1,7 @@
 class Reminder < ApplicationRecord
- belongs_to :user, :class_name => "User"
-
+    belongs_to :user, :class_name => "User"
+    validates :title, presence: true, length: { minimum: 5 }
+    validates :date, presence: true
+    validates :time, presence: true
+    validates :notes, presence: true, length: { minimum: 10 }    
 end
