2c51392c763b040e7d797a724c4fa6df2fecfdcf
Kristopher Heiser <kheiser1@uncc.edu>
2018-11-18 22:48:28 +0000

    Install and configure rspec and capybara

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2018_11_09_194801) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "login"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
