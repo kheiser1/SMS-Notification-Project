79976758ac74f98e3a954da0c6650c233c9c14be
Kristopher Heiser <kheiser1@uncc.edu>
2018-11-26 21:19:24 +0000

    Broken Tests

---
+++ spec/integration/user_adds_a_reminder_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper.rb'
+
+feature "User adds a reminder" do
+    scenario "User successfully signs in." do
+        visit login_path
+        fill_in "email", with: "a@a"
+        fill_in "password", with: "aaaaaaaa"
+        click_button "Submit"
+        expect(page).to have_content("Welcome to the SMS Reminder App")
+    end
+    scenario "User successfully navigates to the new reminders page from the list of reminders page." do
+        visit('/welcome/index')
+        click_link "List of Reminders"
+        expect(page).to have_content("Listing Reminders")
+        click_link "New Reminder"
+        expect(page).to have_content("New Reminder")
+        expect(page).to have_field("Title your reminder")
+        expect(page).to have_field("Enter a date")
+        expect(page).to have_field("Set a time")
+        expect(page).to have_field("Notes")
+    end
+    
+    scenario "User successfully creates a new reminder" do
+        visit new_users_reminders_path
+        expect(page).to have_content("New Ew")
+        fill_in "Title", with: "New Capybara note"
+        fill_in "Text", with: "This is a Capybara note"
+        click_button "Create Note"
+        expect(page).to have_content("New Capybara note")
+        expect(page).to have_content("This is a Capybara note")
+    end
+end
